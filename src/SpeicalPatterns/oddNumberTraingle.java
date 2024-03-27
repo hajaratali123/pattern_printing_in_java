34417a490051f75e2729d7812ea081fef2f9baa1
hajaratali <hazarathalibibijan63@gmail.com>
2024-03-28 00:31:44 +0530

    starcross,starplus,Binary Triangle,floyd's triangle

--- src/SpeicalPatterns/oddNumberTraingle.java
+++ src/SpeicalPatterns/oddNumberTraingle.java
@@ -10,12 +10,38 @@ public class oddNumberTraingle {
         System.out.println("Enter the n :");
         int n = sc.nextInt();
         System.out.println("Odd number triangle :");
+        // By mathematical formula
         for (int i = 1; i <=n; i++) {
             for (int j = 1; j<=i; j++) {
-                System.out.print(2*j-1);
+                System.out.print(2*j-1+" ");
 
             }
             System.out.println();
         }
+        // by changing the increment value
+        System.out.println("by changing the increment value");
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1; j<=2*i-1; j+=2) {
+                System.out.print(j+" ");
+
+            }
+            System.out.println();
+        }
+
+        // by taking another variable as temp
+        System.out.println("by taking another variable as temp");
+
+        for (int i = 1; i <=n; i++) {
+            int temp = 1;
+            for (int j = 1; j<=i; j++) {
+                System.out.print(temp);
+                temp= temp+2;
+
+            }
+            System.out.println();
+        }
+
+
+
     }
 }
